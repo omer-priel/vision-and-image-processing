771995c0ef55253c4fa76338aace151be70f5084
Omer Priel <omer.priel2@gmail.com>
2023-04-02 16:07:57 +0300

    wip

--- src/image_math.py
+++ src/image_math.py
@@ -1,20 +1,26 @@
 # images math operations
 
 import numpy as np
+import scipy
 
-def image_histogram(img: np.ndarray, N: int = 255) -> np.ndarray:
-    hist, colors = np.histogram(img, bins=np.arange(N + 2) - 0.5)
-    colors = colors[:N+1] + 0.5
+def image_histogram(img: np.ndarray, M: int = 255) -> np.ndarray:
+    hist, colors = np.histogram(img, bins=np.arange(M + 2) - 0.5)
+    colors = colors[:M+1] + 0.5
     return hist, colors
 
-def image_histogram_channels(img: np.ndarray, N: int = 255) -> np.ndarray:
-    histR, colorsR = np.histogram(img[:,:,0], bins=np.arange(N + 2) - 0.5)
-    colorsR = colorsR[:N+1] + 0.5
-    
-    histG, colorsG = np.histogram(img[:,:,1], bins=np.arange(N + 2) - 0.5)
-    colorsG = colorsG[:N+1] + 0.5
-    
-    histB, colorsB = np.histogram(img[:,:,2], bins=np.arange(N + 2) - 0.5)
-    colorsB = colorsB[:N+1] + 0.5
-    
+
+def image_histogram_channels(img: np.ndarray, M: int = 255) -> np.ndarray:
+    histR, colorsR = image_histogram(img[:,:,0], M)
+    histG, colorsG = image_histogram(img[:,:,1], M)
+    histB, colorsB = image_histogram(img[:,:,2], M)
+
     return histR, colorsR, histG, colorsG, histB, colorsB
+
+
+def convulsion(img: np.ndarray, conv: np.ndarray):
+    newImg = np.empty(img.shape)
+
+    for dim in range(3):
+        newImg[:,:,dim] = scipy.signal.convolve2d(img[:,:,dim], conv, boundary='symm', mode='same')
+
+    return newImg
--- src/image_transformations.py
+++ src/image_transformations.py
@@ -1,16 +1,25 @@
 # images transformations
 
 import numpy as np
+
 import cv2
+
+from src.image_math import convulsion
 
 def shrink_image(img: np.ndarray, length: int, axis: int = 0) -> np.ndarray:
     fx = fy = length / img.shape[axis]
     return cv2.resize(img, None, fx=fx, fy=fy)
 
 
-def normalize_image(img: np.ndarray, N: int = 255) -> np.ndarray:
-    return img / N
+def normalize_image(img: np.ndarray, M: int = 255) -> np.ndarray:
+    return img / M
 
 
-def unormalize_image(img: np.ndarray, N: int = 255) -> np.ndarray:
-    return np.floor(img * N)
+def unormalize_image(img: np.ndarray, M: int = 255) -> np.ndarray:
+    return (img * M).astype(int)
+
+
+def blur_image(img: np.ndarray, K: int):
+    conv = np.array([1/float(K**2)] * K**2).reshape((K, K))
+
+    return convulsion(img, conv)
